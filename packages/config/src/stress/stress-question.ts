f70a69064d62fd6f9b23be1ec2ea352f700d578d
NSVEGUR <cs20b1016@iiitdm.ac.in>
2025-02-21 09:45:39 +0530

    feat: added remaining 7 questions for stress

--- packages/config/src/stress/stress-question.ts
+++ packages/config/src/stress/stress-question.ts
@@ -48,29 +48,29 @@ export const stressQuestions: Word[][] = [
     { text: "impatient", className: highlightQuestionClass },
     ..."about something?".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
   ],
-  // [
-  //   ..."What makes you a very".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
-  //   ..."special and unique".split(" ").map((text) => ({ text, className: highlightQuestionClass })),
-  //   { text: "person?", className: defaultQuestionClass },
-  // ],
-  // [
-  //   ..."Can you tell us about a time where you were"
-  //     .split(" ")
-  //     .map((text) => ({ text, className: defaultQuestionClass })),
-  //   { text: "scolded/criticized", className: highlightQuestionClass },
-  //   ..."for your".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
-  //   { text: "work?", className: highlightQuestionClass },
-  // ],
-  // [
-  //   ..."What kind of behavior makes you really".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
-  //   { text: "happy?", className: highlightQuestionClass },
-  // ],
-  // [
-  //   ..."What kind of behavior makes you really".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
-  //   { text: "angry/sad?", className: highlightQuestionClass },
-  // ],
-  // [
-  //   ..."What do you when you get".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
-  //   { text: "angry/sad?", className: highlightQuestionClass },
-  // ],
+  [
+    ..."What makes you a very".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
+    ..."special and unique".split(" ").map((text) => ({ text, className: highlightQuestionClass })),
+    { text: "person?", className: defaultQuestionClass },
+  ],
+  [
+    ..."Can you tell us about a time where you were"
+      .split(" ")
+      .map((text) => ({ text, className: defaultQuestionClass })),
+    { text: "scolded/criticized", className: highlightQuestionClass },
+    ..."for your".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
+    { text: "work?", className: highlightQuestionClass },
+  ],
+  [
+    ..."What kind of behavior makes you really".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
+    { text: "happy?", className: highlightQuestionClass },
+  ],
+  [
+    ..."What kind of behavior makes you really".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
+    { text: "angry/sad?", className: highlightQuestionClass },
+  ],
+  [
+    ..."What do you when you get".split(" ").map((text) => ({ text, className: defaultQuestionClass })),
+    { text: "angry/sad?", className: highlightQuestionClass },
+  ],
 ];
